c0e639a8d2e0bc75eea6fdca3dd2e9b468d42ae2
willians3pk <willianpk2010@outlook.com>
2020-09-28 18:34:13 -0300

    atualizado projeto maven

---
+++ SistemaMaven/src/main/java/com/system/sistemamaven/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.system.sistemamaven;
+
+import com.system.conexao.Conexao;
+import com.system.telas.TelaPrincipal;
+import java.util.Date;
+
+/**
+ *
+ * @author willian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+        
+//        Pessoa pessoa = new Pessoa();
+//        pessoa.setNome("VITORIA SILVA DE ALMEIDA");
+//        pessoa.setCpf(883998398);
+//        pessoa.setEmail("vitoria.silva@outlook.com");
+//        pessoa.setDataNascimento(new Date(22072000));
+//        
+//        Endereco end = new Endereco();
+//        end.setRua("Avenda Industrial");
+//        end.setCidade("IMPERATRIZ");
+//        end.setCep(65919230);
+//        end.setComplemento("");
+//        end.setNumeroCasa(713);
+////        end.setPessoas(pessoas);
+//        pessoa.setEndereco(end);
+//        
+//        Conexao banco = new Conexao();
+//        banco.save(end);
+//        banco.save(pessoa);
+    }
+    
+}
