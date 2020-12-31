d15bc5f0c463c69a974b311296df91669f1314d1
willians3pk <willianpk2010@outlook.com>
2020-12-31 15:08:36 -0300

    -----

--- mavenproject/src/main/java/br/com/classes/Main.java
+++ mavenproject/src/main/java/br/com/classes/Main.java
@@ -4,13 +4,11 @@ package br.com.classes;
 
 import br.com.conexao.Conexao;
 
-
 public class Main {
 
     
     public static void main(String[] args) {
        
-        
         Conexao connect = new Conexao();
         Pessoa client = new Pessoa();
         client = null;
